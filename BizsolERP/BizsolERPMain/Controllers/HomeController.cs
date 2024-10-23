77e234dadc75ee316bb0777f2d7ef96ecc75fc63
manoj singh <manoj_243@yahoo.in>
2024-10-23 18:24:36 +0530

    ab

--- BizsolERP/BizsolERPMain/Controllers/HomeController.cs
+++ BizsolERP/BizsolERPMain/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace BizsolERPMain.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View();//asasasassa
         }
 
         public IActionResult Privacy()
