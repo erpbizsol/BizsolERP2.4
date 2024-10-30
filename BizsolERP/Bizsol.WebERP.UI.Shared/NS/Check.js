c6a593f1300788a22fe6af454bbd6a247b4306f5
BIZSOL <166388075+erpbizsol@users.noreply.github.com>
2024-10-30 17:34:01 +0530

    Manoj: 30/10/24 : JS link Example

---
+++ BizsolERP/Bizsol.WebERP.UI.Shared/NS/Check.js
@@ -0,0 +1,9 @@
+﻿function Check() {
+    alert('I am Check by NS folder!');
+}
+function InputText() {
+    var x = document.createElement("INPUT");
+    x.setAttribute("type", "text");
+    x.setAttribute("value", "Hello World!");
+    document.body.appendChild(x);
+}
