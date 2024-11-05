b458018ad6cb88e9a6af67a114ca8a6579f7e1e8
BIZSOL <166388075+erpbizsol@users.noreply.github.com>
2024-11-05 15:11:05 +0530

    Manoj 5/Nov/24 add authKeyExample

---
+++ BizsolERP/Bizsol.WebERP.UI.Shared/wwwroot/js/Pages/CustomControl/_SizeControlPage.js
@@ -0,0 +1,56 @@
+﻿import { SizeControlService } from '../../JSServices/_SizeControlService.js'
+
+class Observable {
+    constructor(subscribe) {
+        this._subscribe = subscribe;
+    }
+    // Subscribe to the observable
+    subscribe(observer) {
+        return this._subscribe(observer);
+    }
+    // Create an observable from an array
+    static fromArray(array) {
+        return new Observable(observer => {
+            array.forEach(item => observer.next(item));
+            observer.complete();
+        });
+    }
+}
+// Example usage
+const observable = new Observable(observer => {
+    // Emit some values
+    observer.next(SizeControlService.GetItemSizeMasterList("GP COIL"));
+   // observer.next(2);
+    //observer.next(3);
+    // Emit an error
+    // observer.error('Something went wrong');
+    // Complete the observable
+    observer.complete();
+    // Cleanup function (optional)
+    return () => {
+        console.log('Observer unsubscribed');
+    };
+});
+
+
+function ME() {
+   // console.log(SizeControlService.GetItemSizeMasterList("GLASSINE"))
+    //console.log(SizeControlService.GetItemSizeMasterList("GP COIL"))
+    //console.log()
+    SizeControlService.GetItemSizeMasterList("GP COIL").then(function (value) { console.log(value) })
+
+    // Subscribe to the observable
+    //const subscription = observable.subscribe({
+    //    next: value => console.log('Received:', value),
+    //    error: err => console.error('Error occurred:', err),
+    //    complete: () => console.log('Observable completed')
+    //});
+    // Unsubscribe (cleanup)
+    //subscription.unsubscribe();
+}
+
+window.ME = ME;
+
+
+
+
--- BizsolERP/BizsolERPMain/Controllers/HomeController.cs
+++ BizsolERP/BizsolERPMain/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json.Linq;
 using System.Diagnostics;
 
 
+
 namespace BizsolERPMain.Controllers
 {
     public class HomeController : Controller
@@ -37,18 +38,20 @@ namespace BizsolERPMain.Controllers
         {
             AuthKey = "{\"ERPDBConStr\":\"Data Source=220.158.165.98,65446;Connection Timeout=0;Persist Security Info=true;Initial Catalog=BizSolERPDBBizDev;User ID=sa;pwd=biz1981;Packet Size=32000\",\"ERPMainDBConStr\":\"data source = 220.158.165.98,65446; initial catalog = BizSolERPMainDB_BizDev; uid = sa; PWD = biz1981; Max Pool Size = 5000\",\"ERPDMSDBConStr\":\"data source = 220.158.165.98,65446; initial catalog = BizSolERPDMSDB_BizDev; uid = sa; PWD = biz1981; Max Pool Size = 5000\",\"ERPDB_Name\":\"BizSolERPDBBizDev\",\"ERPMainDB_Name\":\"BizSolERPMainDB_BizDev\",\"ERPDMSDB_Name\":\"BizSolERPDMSDB_BizDev\",\"AuthToken\":\"xyz\",\"UserMaster_Code\":\"145\",\"CompanyCode\":\"104\",\"CrystalReportBaseUrl\":\"https://web.bizsol.in/CRReports/Reports/Report.aspx?\"}";
 
-            AuthKeySession? AuthData = System.Text.Json.JsonSerializer.Deserialize<AuthKeySession>(AuthKey);
+            ViewBag.AuthKey = AuthKey;
 
-            HttpContext.Session.SetString("ConnString", AuthData.ERPDBConStr.ToString());
-            HttpContext.Session.SetString("MainDBConnString", AuthData.ERPMainDBConStr.ToString());
-            HttpContext.Session.SetString("DMSDBConnString", AuthData.ERPDMSDBConStr.ToString());
-            HttpContext.Session.SetString("ERPDBName", AuthData.ERPDB_Name.ToString());
-            HttpContext.Session.SetString("MainDBName", AuthData.ERPMainDB_Name.ToString());
-            HttpContext.Session.SetString("DMSDBName", AuthData.ERPDMSDB_Name.ToString());
-            HttpContext.Session.SetString("AuthToken", AuthData.AuthToken.ToString());
-            HttpContext.Session.SetString("CompanyCode", AuthData.CompanyCode.ToString());
-            HttpContext.Session.SetString("UserMaster_Code", AuthData.UserMaster_Code.ToString());
-            HttpContext.Session.SetString("CrystalReportBaseUrl", AuthData.CrystalReportBaseUrl.ToString());
+            //AuthKeySession? AuthData = System.Text.Json.JsonSerializer.Deserialize<AuthKeySession>(AuthKey);
+
+            //HttpContext.Session.SetString("ConnString", AuthData.ERPDBConStr.ToString());
+            //HttpContext.Session.SetString("MainDBConnString", AuthData.ERPMainDBConStr.ToString());
+            //HttpContext.Session.SetString("DMSDBConnString", AuthData.ERPDMSDBConStr.ToString());
+            //HttpContext.Session.SetString("ERPDBName", AuthData.ERPDB_Name.ToString());
+            //HttpContext.Session.SetString("MainDBName", AuthData.ERPMainDB_Name.ToString());
+            //HttpContext.Session.SetString("DMSDBName", AuthData.ERPDMSDB_Name.ToString());
+            //HttpContext.Session.SetString("AuthToken", AuthData.AuthToken.ToString());
+            //HttpContext.Session.SetString("CompanyCode", AuthData.CompanyCode.ToString());
+            //HttpContext.Session.SetString("UserMaster_Code", AuthData.UserMaster_Code.ToString());
+            //HttpContext.Session.SetString("CrystalReportBaseUrl", AuthData.CrystalReportBaseUrl.ToString());
 
         }
 
