19f54d9c198bfd16aa1709c252551cf27918983a
BIZSOL <166388075+erpbizsol@users.noreply.github.com>
2024-11-12 11:27:27 +0530

    Manoj Change 12/11/24 : Size and BizSol InputControl

---
+++ BizsolERP/Bizsol.WebERP.UI.Shared/Controllers/CustomControlController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bizsol.WebERP.UI.Shared.Controllers
+{
+    public class CustomControlController : Controller
+    {
+        // GET: CustomControlController
+        public ActionResult SizeControl(int ItemMaster_Code, int ItemSizeMaster_Code, string CallBackFunctionName_btnDone, string EditParameterList, string CallBackFunctionName_btnClose)
+        {
+            
+            ViewBag.ItemMaster_Code = ItemMaster_Code;
+            ViewBag.ItemSizeMaster_Code = ItemSizeMaster_Code;
+            ViewBag.CallBackFunctionName_btnDone = CallBackFunctionName_btnDone;
+            ViewBag.CallBackFunctionName_btnClose = CallBackFunctionName_btnClose;
+            ViewBag.EditParameterList = EditParameterList;
+            return PartialView("_SizeControl");
+            
+        }
+
+    }
+}
