5790f311dce322086c8569095da25d2ce3aab167
Neeraj <neerajdwi1998@gmail.com>
2024-11-15 16:13:27 +0530

    merge antu 15-Nov-24

--- BizsolERP/Bizsol.WebERP.UI.Shared/wwwroot/js/PromiseAjaxCallApi.js
+++ BizsolERP/Bizsol.WebERP.UI.Shared/wwwroot/js/PromiseAjaxCallApi.js
@@ -11,21 +11,21 @@
                 data: Data,
                 dataType: 'json',
                 success: function (response) {
+                    resolve(response);
+                    //if (response.status === "Success") {
+                    //resolve(response);
 
-                    if (response.status === "Success") {
-                        resolve(response);
-
-                    } else if (response.message == 'No Record Found') {
-                        alert("Error: No Record Found");
-                    }
-                    else if (response.length > 0) {
-                        //console.log(response);
-                        //return response;
-                        resolve(response);
-                    }
-                    else {
-                        alert("Error: " + response.message);
-                    }
+                    //} else if (response.message == 'No Record Found') {
+                    //    alert("Error: No Record Found");
+                    //}
+                    //else if (response.length > 0) {
+                    //    //console.log(response);
+                    //    //return response;
+                    //    resolve(response);
+                    //}
+                    //else {
+                    //    alert("Error: " + response.message);
+                    //}
                 },
                 error: function (xhr, status, error) {
                     var errorMessage = status + ': ' + error;
