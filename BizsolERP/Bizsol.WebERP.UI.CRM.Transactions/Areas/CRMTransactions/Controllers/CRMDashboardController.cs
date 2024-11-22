f47795bf726acce44bb723bf5a26ca743a905b84
BIZSOL <166388075+erpbizsol@users.noreply.github.com>
2024-11-22 18:24:11 +0530

    Manoj CheckinCheckout Change 22-Nov-24

---
+++ BizsolERP/Bizsol.WebERP.UI.CRM.Transactions/Areas/CRMTransactions/Controllers/CRMDashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bizsol.WebERP.UI.CRM.Transactions.Areas.CRMTransactions.Controllers
+{
+    [Area("CRMTransactions")]
+    public class CRMDashboardController : Controller
+    {
+       
+        public IActionResult CRMDashboard()
+        {
+            return View();
+        }
+    }
+
+}
