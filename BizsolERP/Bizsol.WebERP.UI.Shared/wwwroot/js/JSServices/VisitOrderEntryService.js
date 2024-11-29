3e3eab31ec9f8f5bbaaee428a33a00ac95109f73
manoj singh <manoj_243@yahoo.in>
2024-11-29 10:26:18 +0530

    Ankit Visit 29/11/2024

--- BizsolERP/Bizsol.WebERP.UI.Shared/wwwroot/js/JSServices/VisitOrderEntryService.js
+++ BizsolERP/Bizsol.WebERP.UI.Shared/wwwroot/js/JSServices/VisitOrderEntryService.js
@@ -1,2 +1,54 @@
 ﻿import { UrlService } from '../URL.js';
 import { promiseAjaxCallApi } from '../PromiseAjaxCallApi.js';
+
+const VisitOrderEntryService = {
+    GetNestedMarketingManList: function GetNestedMarketingManList() {
+        var URL = UrlService.API_ENDPOINT_SALESPERSON + `/GetNestedMarketingManList`;
+        return promiseAjaxCallApi.CallAPI('GET', URL, "").then(
+            function (value) {
+                return value;
+            }
+        );
+    },
+
+    GetVisitMasterList: function GetVisitMasterList(FromDate, ToDate, MarketingMan) {
+        var authKeyData = JSON.parse(sessionStorage.getItem('authKey'));
+        var userMasterCode = authKeyData.UserMaster_Code;
+        var URL = UrlService.API_ENDPOINT_VISIT_MASTER + `/GetVerifiedRoutePlanUserAndDateWise?User_ID=${userMasterCode}&MarketingMan_Name=${MarketingMan}&FromDate=${FromDate}&ToDate=${ToDate}`;
+        return promiseAjaxCallApi.CallAPI('GET', URL, "").then(
+            function (value) {
+                return value;
+            }
+        );
+    },
+    GetRoutePlanList: function GetRoutePlanList() {
+        var URL = UrlService.API_ENDPOINT_ROUTE_PLAN + `/GetRoutePlanList`;
+        return promiseAjaxCallApi.CallAPI('GET', URL, "").then(
+            function (value) {
+                return value;
+            }
+        );
+    },
+    NotVisitedRoutePlan: function NotVisitedRoutePlan(RoutePlanMaster_Code, Reason) {
+        var authKeyData = JSON.parse(sessionStorage.getItem('authKey'));
+        var userMasterCode = authKeyData.UserMaster_Code;
+        var URL = UrlService.API_ENDPOINT_VISIT_MASTER + `/NotVisitedRoutePlan?RoutePlanMaster_Code=${RoutePlanMaster_Code}&UserMaster_Code=${userMasterCode}&ReasonForClose=${Reason}`;
+        return promiseAjaxCallApi.CallAPI('POST', URL, "").then(
+            function (value) {
+                return value;
+            }
+        );
+    },
+    CheckInVisit: function CheckInVisit(RoutePlanMaster_Code, CheckIn, location, ChekedInLocation) {
+        var authKeyData = JSON.parse(sessionStorage.getItem('authKey'));
+        var userMasterCode = authKeyData.UserMaster_Code;
+        var URL = UrlService.API_ENDPOINT_VISIT_MASTER + `/CheckInVisit?RoutePlanMaster_Code=${RoutePlanMaster_Code}&CheckIn=${CheckIn}&Location=${location}&ChekedInLocation=${ChekedInLocation}&UserMaster_Code=${userMasterCode}`;
+        return promiseAjaxCallApi.CallAPI('POST', URL, "").then(
+            function (value) {
+                return value;
+            }
+        );
+    },
+}
+
+export { VisitOrderEntryService }
