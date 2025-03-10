4fb5bda1d1f6db5f01a835fd428ab5326daade8b
BIZSOL <166388075+erpbizsol@users.noreply.github.com>
2025-03-10 12:08:48 +0530

    Neeraj and Manoj RollingChange

---
+++ BizsolERP/Bizsol.ERP.UI.Production.Masters/Areas/ProductionMasters/Controllers/RollingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bizsol.WebERP.UI.Production.Transactions.Areas.ProductionTransactions.Controllers
+{
+    [Area("ProductionMasters")]
+    public class RollingController : Controller
+    {
+        public IActionResult RollingProductionEntry()
+        {
+            return View();
+        }
+        public IActionResult RollingProductionSummary()
+        {
+            return View();
+        }
+    }
+}
