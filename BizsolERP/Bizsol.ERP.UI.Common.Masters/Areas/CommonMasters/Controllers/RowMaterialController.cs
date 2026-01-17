2cd877b17ffbb500d593936bb52528274b50902c
manoj singh <manoj_243@yahoo.in>
2026-01-17 13:29:59 +0530

    RowMaterial ,RmOffer and RmClearance 17-Jan-2026 Ankit

---
+++ BizsolERP/Bizsol.ERP.UI.Common.Masters/Areas/CommonMasters/Controllers/RowMaterialController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bizsol.WebERP.UI.Common.Masters.Areas.CommonMasters.Controllers
+{
+    [Area("CommonMasters")]
+    public class RowMaterialController : Controller
+    {
+        public IActionResult RMOffer()
+        {
+            return View();
+        }
+        public IActionResult RMClearanceEntry()
+        {
+            return View();
+        }
+    }
+}
