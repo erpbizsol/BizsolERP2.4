a083360a32d0bc408c1318f3422cdd27ee614085
Neeraj <neerajdwi1998@gmail.com>
2026-01-21 18:22:10 +0530

    RMInspection Clearance Entry 21-Jan-2026 Neeraj

--- BizsolERP/Bizsol.WebERP.UI.Shared/wwwroot/js/JSServices/_RawMaterialOfferService.js
+++ BizsolERP/Bizsol.WebERP.UI.Shared/wwwroot/js/JSServices/_RawMaterialOfferService.js
@@ -17,7 +17,33 @@ const RawMaterialOfferService = {
                 return value;
             }
         );
-    }
+    },
+    GetRawMaterialClearanceList: function GetRawMaterialClearanceList(AccountMaster_Code, OrderNo, ProjectNo) {
+        var URL = UrlService.API_ENDPOINT_RawMaterialOffer + "/GetRawMaterialClearanceList?AccountMaster_Code=" + AccountMaster_Code + "&OrderNo=" + OrderNo + "&ProjectNo=" + ProjectNo;
+        return promiseAjaxCallApi.CallAPI('GET', URL, "").then(
+            function (value) {
+                return value;
+            }
+        );
+    },
+    GetRawMaterialClearanceVerify: function GetRawMaterialClearanceVerify(Code) {
+        let userCode = JSON.parse(sessionStorage.getItem('authKey')).UserMaster_Code;
+        var URL = UrlService.API_ENDPOINT_RawMaterialOffer + "/GetRawMaterialClearanceVerify?Code=" + Code + "&UserMaster_Code=" + userCode;
+        return promiseAjaxCallApi.CallAPI('POST', URL, "").then(
+            function (value) {
+                return value;
+            }
+        );
+    },
+    GetRawMaterialClearanceReject: function GetRawMaterialClearanceReject(Code, RejectRemark) {
+        let userCode = JSON.parse(sessionStorage.getItem('authKey')).UserMaster_Code;
+        var URL = UrlService.API_ENDPOINT_RawMaterialOffer + "/GetRawMaterialClearanceReject?Code=" + Code + "&RejectRemark=" + RejectRemark + "&UserMaster_Code=" + userCode;
+        return promiseAjaxCallApi.CallAPI('POST', URL, "").then(
+            function (value) {
+                return value;
+            }
+        );
+    },
 }
 
 export { RawMaterialOfferService }
