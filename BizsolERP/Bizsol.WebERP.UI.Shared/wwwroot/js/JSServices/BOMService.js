73f99fba24e3b0abad17935d27fb0eb3b39cfb9f
Neeraj <neerajdwi1998@gmail.com>
2026-03-10 16:57:53 +0530

    BOM neeraj 10-march-2026

--- BizsolERP/Bizsol.WebERP.UI.Shared/wwwroot/js/JSServices/BOMService.js
+++ BizsolERP/Bizsol.WebERP.UI.Shared/wwwroot/js/JSServices/BOMService.js
@@ -4,9 +4,31 @@ import { promiseAjaxCallApi } from '../PromiseAjaxCallApi.js';
 
 const BOMService = {
     GetBOMList: function GetBOMList() {
-        var authKeyData = JSON.parse(sessionStorage.getItem('authKey'));
-        var userMasterCode = authKeyData.UserMaster_Code;
-        var URL = UrlService.API_ENDPOINT_BOM + "/GetBOMList?UserMaster_Code=" + userMasterCode;
+        var URL = UrlService.API_ENDPOINT_BOM + "/GetBOMList";
+        return promiseAjaxCallApi.CallAPI('GET', URL, "").then(
+            function (value) {
+                return value;
+            }
+        );
+    },
+    GetCategoryList: function GetCategoryList() {
+        var URL = UrlService.API_ENDPOINT_BOM + "/GetCategoryList";
+        return promiseAjaxCallApi.CallAPI('GET', URL, "").then(
+            function (value) {
+                return value;
+            }
+        );
+    },
+    GetWorkTypeList: function GetWorkTypeList() {
+        var URL = UrlService.API_ENDPOINT_BOM + "/GetWorkTypeList";
+        return promiseAjaxCallApi.CallAPI('GET', URL, "").then(
+            function (value) {
+                return value;
+            }
+        );
+    },
+    GetItemMasterList: function GetItemMasterList(WorkType) {
+        var URL = UrlService.API_ENDPOINT_BOM + "/GetItemMasterList?WorkType=" + WorkType;
         return promiseAjaxCallApi.CallAPI('GET', URL, "").then(
             function (value) {
                 return value;
@@ -15,7 +37,7 @@ const BOMService = {
     },
 
     SaveBOMRow: function SaveBOMRow(data) {
-        var URL = UrlService.API_ENDPOINT_BOM + "/SaveBOMRow";
+        var URL = UrlService.API_ENDPOINT_BOM + "/SaveBOM";
         return promiseAjaxCallApi.CallAPI('POST', URL, JSON.stringify(data)).then(function (value) {
             return value;
         });
@@ -24,6 +46,14 @@ const BOMService = {
     VerifyBOMRow: function VerifyBOMRow(data) {
         var URL = UrlService.API_ENDPOINT_BOM + "/VerifyBOMRow";
         return promiseAjaxCallApi.CallAPI('POST', URL, JSON.stringify(data)).then(function (value) {
+            return value;
+        });
+    },
+
+    DeleteBOM: function DeleteBOM(code, reason) {
+        let userCode = JSON.parse(sessionStorage.getItem('authKey')).UserMaster_Code;
+        var URL = UrlService.API_ENDPOINT_BOM + "/DeleteBOM?Code=" + code + "&UserMaster_Code=" + userCode + "&ReasonForDelete=" + encodeURIComponent(reason || '') + "&IPAddress=1&Location=1";
+        return promiseAjaxCallApi.CallAPI('POST', URL, "").then(function (value) {
             return value;
         });
     },
