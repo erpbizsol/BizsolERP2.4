87199ab91f361caa5ee73b2299dd16216f362ad5
BIZSOL <166388075+erpbizsol@users.noreply.github.com>
2026-03-12 18:36:31 +0530

    Add PO Level Approval module & user rights to SubProject
    Introduced PO Level Approval feature with new Razor page, UI, and backend service for reviewing and approving POs by level. Enhanced Sub Project Master to support assigning users with rights to verify PO, including Select2 multiselect UI and backend integration. Updated service patterns and URL management for consistency. Improved UI/UX with responsive layouts and approval flow steppers.

--- BizsolERP/Bizsol.WebERP.UI.Shared/wwwroot/js/URL.js
+++ BizsolERP/Bizsol.WebERP.UI.Shared/wwwroot/js/URL.js
@@ -131,6 +131,7 @@ let UrlService = {
     API_ENDPOINT_BOM: `${environment.BASE_URL}/BOM`,
     API_ENDPOINT_PurchaseOrderMaster: `${environment.BASE_URL}/PurchaseOrderMaster`,
     API_ENDPOINT_POApprovalConfig: `${environment.BASE_URL}/POApprovalConfig`,
+    API_ENDPOINT_POLevelsApprove: `${environment.BASE_URL}/POLevelApprove`,
 }
 
 export { UrlService  }
