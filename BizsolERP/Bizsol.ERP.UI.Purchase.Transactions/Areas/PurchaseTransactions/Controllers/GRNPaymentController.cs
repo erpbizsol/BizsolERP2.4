83a2310858a4d546ef1bd235b64b1e76beafa765
antu <antu123.164@gmail.com>
2026-03-24 14:38:13 +0530

    antu-24-03-2026

---
+++ BizsolERP/Bizsol.ERP.UI.Purchase.Transactions/Areas/PurchaseTransactions/Controllers/GRNPaymentController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bizsol.WebERP.UI.Purchase.Transactions.Areas.PurchaseTransactions.Controllers
+{
+    [Area("PurchaseTransactions")]
+    public class GRNPaymentController : Controller
+    {
+        public IActionResult GRNPaymentApprovalConfiguration()
+        {
+            return View();
+        }
+    }
+}
