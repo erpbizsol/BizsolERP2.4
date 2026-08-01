114c6edb9553655cda9313fabc045f689f41b0de
neeraj <neeraj@bizsol.in>
2026-08-01 11:50:00 +0530

    Expense Entry,BOM and GRN Service 01-Aug-2026 Neeraj

--- BizsolERP/Bizsol.WebERP.UI.CRM.Transactions/wwwroot/js/ExpenseHeadMaster.js
+++ BizsolERP/Bizsol.WebERP.UI.CRM.Transactions/wwwroot/js/ExpenseHeadMaster.js
@@ -8,6 +8,7 @@ let G_ExpenseHeadMaster = 0;
 let G_Code = 0;
 let G_ExpenseHeadMaster_Mode = 'E';
 let G_EHM_DeleteCode = 0;
+let G_ExpenseGroupMaster_Code = 0;
 
 function getFinancialYear() {
     return BizSolHelperFunction.getFinancialYear();
@@ -28,7 +29,7 @@ function GetExpenseHeadMasterTable(){
             const Button = false;
             const showButtons = [];
             const StringdoubleFilterColumn = [];
-            const hiddenColumns = ["Code"];
+            const hiddenColumns = ["Code", "ExpenseGroupMaster_Code"];
             const ColumnAlignment = {};
 
             const updatedResponse = response.map(item => {
@@ -79,17 +80,19 @@ function EditData(Code, desp, Mode) {
     G_ExpenseHeadMaster = Code;
     G_ExpenseHeadMaster_Mode = Mode || 'E';
     if (Mode === 'V') {
-        CreateNew_ExpenseHeadMaster();
+        CreateNew_ExpenseHeadMaster(true);
         $('#txtExpenseDescription').val(desp);
-        $('#newCreateForm input').prop('disabled', true);
+        $('#newCreateForm input, #newCreateForm select').prop('disabled', true);
         $('#chkIsKMApplicable').prop('disabled', true);
+        $('#ddlExpenseGroup').prop('disabled', true);
         $('#newCreateFormExpenseHeadLimit').hide();
         GetExpenseHeadMasterByCode(G_ExpenseHeadMaster, Mode);
         $('.EditButtonLimitData').prop('disabled', true);
     } else {
-        CreateNew_ExpenseHeadMaster();
-        $('#newCreateForm input').prop('disabled', false);
+        CreateNew_ExpenseHeadMaster(true);
+        $('#newCreateForm input, #newCreateForm select').prop('disabled', false);
         $('#chkIsKMApplicable').prop('disabled', false);
+        $('#ddlExpenseGroup').prop('disabled', false);
         $('#txtExpenseDescription').val(desp);
         GetExpenseHeadMasterByCode(G_ExpenseHeadMaster, Mode);
     }
@@ -99,12 +102,25 @@ function GetExpenseHeadMasterByCode(G_ExpenseHeadMaster, Mode) {
         return false;
     }
     ExpenseHeadMasterService.GetExpenseHeadMasterByCode(G_ExpenseHeadMaster).then(function (response) {
+        var masterList = response?.ExpenseHeadMasterForEdit || [];
+        var limitList = response?.ExpenseHeadLimitDetailsForEdit || [];
+
+        if (masterList && masterList.length > 0) {
+            var master = masterList[0];
+            var expenseDesp = master.ExpenseDesp || master.expenseDesp || '';
+            var expenseGroupCode = master.ExpenseGroupMaster_Code ?? master.expenseGroupMaster_Code ?? 0;
+            if (expenseDesp) {
+                $('#txtExpenseDescription').val(expenseDesp);
+            }
+            G_ExpenseGroupMaster_Code = parseInt(expenseGroupCode, 10) || 0;
+            GetExpenseGroupList(G_ExpenseGroupMaster_Code);
+        }
+
         var $limitRow = $("#rowExpenseHeadLimitDetails");
         var $limitCard = $("#cardExpenseHeadLimitDetails");
-        if (response && response.length > 0) {
+        if (limitList && limitList.length > 0) {
             $limitRow.show();
             $limitCard.show();
-            const data = response; 
             const StringFilterColumn = [];
             const NumericFilterColumn = [];
             const DateFilterColumn = [];
@@ -116,7 +132,7 @@ function GetExpenseHeadMasterByCode(G_ExpenseHeadMaster, Mode) {
                 "Per Day Limit": 'right',
                 "Effective From": 'center',
             };
-            const updatedResponse = response.map(item => {
+            const updatedResponse = limitList.map(item => {
                 const kmFlag = item?.["Is KM Applicable"] || 'N';
                 let buttonsHTML = `<button class="btn btn-primary icon-height mb-1 EditButtonLimitData" title="Edit Limit" onclick="EditLimitData('${item?.["Expense Category"]}','${item?.["Effective From"]}','${item?.["Per Day Limit"]}','${kmFlag}')"><i class="fa fa-pencil"></i></button>&nbsp`;
                 return {
@@ -127,17 +143,6 @@ function GetExpenseHeadMasterByCode(G_ExpenseHeadMaster, Mode) {
 
             BizsolCustomFilterGrid.CreateDataTable("table-header-ExpenseHeadLimitDetails", "table-body-ExpenseHeadLimitDetails", updatedResponse, Button, showButtons, StringFilterColumn, NumericFilterColumn, DateFilterColumn, StringdoubleFilterColumn, hiddenColumns, ColumnAlignment);
             $("#paginator-ExpenseHeadLimitDetails").show();
-            //if (data.DesignationName) {
-            //    BizSolHelperFunction.SelectOptionByText('txtDesignation', data.DesignationName);
-            //}
-
-            //if (data.EffectiveFrom) {
-            //    $('#txtEffectiveDate').val(data.EffectiveFrom);
-            //}
-
-            //if (data.PerDayLimit !== undefined && data.PerDayLimit !== null) {
-            //    $('#txtPerDayLimit').val(data.PerDayLimit);
-            //}
             if (Mode === 'V') {
                 $('.EditButtonLimitData').prop('disabled', true);
             }
@@ -161,13 +166,21 @@ function EditLimitData(desp, effectiveDate, parDayLimit, isKMApplicable) {
     $('#chkIsKMApplicable').prop('checked', isKM);
     ToggleKMApplicableLabel();
 }
-function CreateNew_ExpenseHeadMaster() {
-    G_ExpenseHeadMaster_Mode = 'E';
+function CreateNew_ExpenseHeadMaster(skipExpenseGroupLoad) {
+    if (!skipExpenseGroupLoad) {
+        G_ExpenseHeadMaster = 0;
+        G_ExpenseHeadMaster_Mode = 'E';
+        G_ExpenseGroupMaster_Code = 0;
+        $('#txtExpenseDescription').val('');
+    }
     $('#locateExpenseHeadMaster').hide();
     $('#newCreateForm').show();
     $('#newCreateFormExpenseHeadLimit').show();
-    $('#newCreateForm input').prop('disabled', false);
+    $('#newCreateForm input, #newCreateForm select').prop('disabled', false);
     GetDESIGNATIONAMEList();
+    if (!skipExpenseGroupLoad) {
+        GetExpenseGroupList(0);
+    }
     var today = new Date();
     const yyyy = today.getFullYear();
     const mm = (today.getMonth() + 1).toString().padStart(2, '0');
@@ -179,6 +192,7 @@ function CreateNew_ExpenseHeadMaster() {
 }
 function ExpenseHeadMaster_Back() {
     G_ExpenseHeadMaster = 0;
+    G_ExpenseGroupMaster_Code = 0;
     $('#locateExpenseHeadMaster').show();
     $('#newCreateForm').hide();
     $('#newCreateFormExpenseHeadLimit').hide();
@@ -202,6 +216,35 @@ function GetDESIGNATIONAMEList() {
         toastr.error('Error fetching user list:', error);
     });
 }
+function GetExpenseGroupList(selectedCode) {
+    ExpenseHeadMasterService.GetExpenseGroupList().then(function (response) {
+        if (response && response.length > 0) {
+            BindSelectList($('#ddlExpenseGroup')[0], response.map((item) => ({ Code: item.Code, Desp: item.Desp })));
+            $('#ddlExpenseGroup').select2({
+                width: '-webkit-fill-available'
+            });
+            var codeToSelect = selectedCode || G_ExpenseGroupMaster_Code || 0;
+            SetExpenseGroupValue(codeToSelect);
+            if (G_ExpenseHeadMaster_Mode === 'V') {
+                $('#ddlExpenseGroup').prop('disabled', true);
+            }
+        } else {
+            BindSelectList($('#ddlExpenseGroup')[0], []);
+            toastr.warning('No Expense Group found.');
+        }
+    }).catch(function () {
+        toastr.error('Error fetching Expense Group list.');
+    });
+}
+function SetExpenseGroupValue(code) {
+    var value = parseInt(code, 10) || 0;
+    if ($('#ddlExpenseGroup option').length === 0) {
+        G_ExpenseGroupMaster_Code = value;
+        return;
+    }
+    $('#ddlExpenseGroup').val(String(value)).trigger('change');
+    G_ExpenseGroupMaster_Code = value;
+}
 function BindSelectList(element, list) {
     let option = '<option value="0"></option>';
     $.each(list, function (key, val) {
@@ -212,11 +255,16 @@ function BindSelectList(element, list) {
 function ClearFormData() {
     $('#txtPerDayLimit').val('');
     $('#chkIsKMApplicable').prop('checked', false);
+    G_ExpenseGroupMaster_Code = 0;
+    if ($('#ddlExpenseGroup').length) {
+        $('#ddlExpenseGroup').val('0').trigger('change');
+    }
     ToggleKMApplicableLabel();
 }
 function submit_ExpenseHeadMaster() {
     let code = G_ExpenseHeadMaster;
     let ExpenseDescription = $('#txtExpenseDescription').val();
+    let expenseGroupCode = parseInt($('#ddlExpenseGroup').val(), 10) || 0;
     let designationCode = $('#txtDesignation').val();
     let G_Date = $('#txtEffectiveDate').val();
     let PerDayLimit = parseFloat($('#txtPerDayLimit').val()) || 0;
@@ -227,6 +275,10 @@ function submit_ExpenseHeadMaster() {
     }
     if (!ExpenseDescription) {
         toastr.warning('Please Fill The Expense Description.');
+        return;
+    }
+    if (!expenseGroupCode || expenseGroupCode === 0) {
+        toastr.warning('Please Select Expense Group.');
         return;
     }
 
@@ -245,7 +297,8 @@ function submit_ExpenseHeadMaster() {
         expenseHeadMaster: [
             {
                 code: parseInt(code),
-                expenseDesp: ExpenseDescription
+                expenseDesp: ExpenseDescription,
+                expenseGroupMaster_Code: expenseGroupCode
             }
         ],
         expenseHeadLimitDetails: objExpenseHeadLimitDetails 
