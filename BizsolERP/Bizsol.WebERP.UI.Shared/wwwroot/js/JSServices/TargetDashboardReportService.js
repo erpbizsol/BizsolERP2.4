58916e42e8e157c9bf1362bd0872f4903c185706
antu <antu123.164@gmail.com>
2026-09-22 11:29:05 +0530

    antu-22-2

---
+++ BizsolERP/Bizsol.WebERP.UI.Shared/wwwroot/js/JSServices/TargetDashboardReportService.js
@@ -0,0 +1,24 @@
+﻿import { UrlService } from '../URL.js';
+import { promiseAjaxCallApi } from '../PromiseAjaxCallApi.js';
+
+const TargetDashboardReportService = {
+    
+    GetDashboard: function GetDashboard(FromDate, ToDate, Code, FinYear) {
+        const base = UrlService.API_ENDPOINT_DEALER_TARGET_MASTER;
+        const URL =
+            base +
+            '/GetTargetDashboardReport?FromDate=' +
+            encodeURIComponent(FromDate == null ? '' : FromDate) +
+            '&ToDate=' +
+            encodeURIComponent(ToDate == null ? '' : ToDate) +
+            '&Code=' +
+            encodeURIComponent(Code == null ? 0 : Code) +
+            '&FinYear=' +
+            encodeURIComponent(FinYear == null ? '' : FinYear);
+        return promiseAjaxCallApi.CallAPI('GET', URL, '', { suppressErrorToast: true }).then(function (value) {
+            return value;
+        });
+    },
+};
+
+export { TargetDashboardReportService };
